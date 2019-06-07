07112670395c81181e81f54299ff628bc27e594e
yoichi.kaneko <yoichi.kaneko@classi.jp>
2019-06-07 10:24:49 +0900

    コンフィグ情報を外部ファイルに分離

---
+++ public/js/page_config.js
@@ -0,0 +1,48 @@
+var page={
+        "about": {
+            "js": "page-1.js",
+            "tab": "tab-1",
+            "html": "page-1.html",
+            "main": 1,
+            "title": "Cascade - Personal vCard Template - About Page"
+        },
+        "portfolio": {
+            "js": "page-2.js",
+            "tab": "tab-2",
+            "html": "page-2.html",
+            "main": 1,
+            "title": "Cascade - Personal vCard Template - Portfolio Page"
+        },
+        "resume": {
+            "js": "page-3.js",
+            "tab": "tab-3",
+            "html": "page-3.html",
+            "main": 1,
+            "title": "Cascade - Personal vCard Template - Resume Page"
+        },
+        "interests": {
+            "js": "page-4.js",
+            "tab": "tab-4",
+            "html": "page-4.html",
+            "main": 1,
+            "title": "Cascade - Personal vCard Template - Interests Page"
+        },
+        "contact": {
+            "js": "page-5.js",
+            "tab": "tab-5",
+            "html": "page-5.html",
+            "main": 1,
+            "title": "Cascade - Personal vCard Template - Contact Page"
+        }
+    };
+
+var options={
+        "openStart": "",
+        "title": "Cascade - Personal vCard Template",
+        "meta": {
+            "keywords": "black, clean, creative, curriculum, curriculum vitae, cv, dark, minimalist, personal, personal profile, personal vcard, portfolio, resume, vcard, virtual card",
+            "description": "Cascade is a minimalist personal vCard template based on four vertical menu tabs. The first tab is a typical descriptive about page with slider, the second is a gallery with build-in lightbox, next tab is a personal info page and the last tab is the contact page with working form."
+        }
+    };
+
+var request='';
