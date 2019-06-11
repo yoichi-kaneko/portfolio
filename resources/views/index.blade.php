27ef3d8fe10cbf22fc402b2d45da320b7048dc05
yoichi.kaneko <yoichi.kaneko@classi.jp>
2019-06-11 16:15:26 +0900

    privateページの調整途中

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,188 +1,162 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja">
-    <head>
-        <title>Yoichi Kaneko's Portfolio Page</title>
-        <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
-        <meta name="keywords" content="portfolio, developer, php, freelance"/>
-        <meta name="description" content="金子陽一のポートフォリオページ"/>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+    <title>Yoichi Kaneko's Portfolio Page</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+    <meta name="keywords" content="portfolio, developer, php, freelance"/>
+    <meta name="description" content="金子陽一のポートフォリオページ"/>
 
-        {{Html::style('css/nivo-slider.css')}}
-        {{Html::style('css/jquery.qtip.css')}}
-        {{Html::style('css/jquery.captify.css')}}
-        {{Html::style('css/jquery.jScrollPane.css')}}
-        {{Html::style('css/fancybox/jquery.fancybox.css')}}
-        {{Html::style('css/fancybox/helpers/jquery.fancybox-buttons.css')}}
-        {{Html::style('css/base.css')}}
-        {{Html::style('css/page.css')}}
+    {{Html::style('css/nivo-slider.css')}}
+    {{Html::style('css/jquery.qtip.css')}}
+    {{Html::style('css/jquery.captify.css')}}
+    {{Html::style('css/jquery.jScrollPane.css')}}
+    {{Html::style('css/fancybox/jquery.fancybox.css')}}
+    {{Html::style('css/fancybox/helpers/jquery.fancybox-buttons.css')}}
+    {{Html::style('css/base.css')}}
+    {{Html::style('css/page.css')}}
 
-        {{Html::style('css/responsive/width-0-959.css', ['media' => 'screen and (min-width:0px) and (max-width:959px)'])}}
-        {{Html::style('css/responsive/width-0-767.css', ['media' => 'screen and (min-width:0px) and (max-width:767px)'])}}
-        {{Html::style('css/responsive/width-480-959.css', ['media' => 'screen and (min-width:480px) and (max-width:959px)'])}}
+    {{Html::style('css/responsive/width-0-959.css', ['media' => 'screen and (min-width:0px) and (max-width:959px)'])}}
+    {{Html::style('css/responsive/width-0-767.css', ['media' => 'screen and (min-width:0px) and (max-width:767px)'])}}
+    {{Html::style('css/responsive/width-480-959.css', ['media' => 'screen and (min-width:480px) and (max-width:959px)'])}}
 
-        {{Html::style('css/responsive/width-768-959.css', ['media' => 'screen and (min-width:768px) and (max-width:959px)'])}}
-        {{Html::style('css/responsive/width-480-767.css', ['media' => 'screen and (min-width:480px) and (max-width:767px)'])}}
-        {{Html::style('css/responsive/width-0-479.css', ['media' => 'screen and (min-width:0px) and (max-width:479px)'])}}
+    {{Html::style('css/responsive/width-768-959.css', ['media' => 'screen and (min-width:768px) and (max-width:959px)'])}}
+    {{Html::style('css/responsive/width-480-767.css', ['media' => 'screen and (min-width:480px) and (max-width:767px)'])}}
+    {{Html::style('css/responsive/width-0-479.css', ['media' => 'screen and (min-width:0px) and (max-width:479px)'])}}
 
-        <script type="text/javascript">
-            var mainURL='';
-        </script>
+    {{Html::script('js/linkify.js')}}
+    {{Html::script('js/jquery.min.js')}}
+    {{Html::script('js/jquery-migrate.min.js')}}
+    {{Html::script('js/jquery.easing.js')}}
+    {{Html::script('js/jquery.isotope.js')}}
+    {{Html::script('js/jquery.captify.js')}}
+    {{Html::script('js/jquery.blockUI.js')}}
+    {{Html::script('js/jquery.qtip.min.js')}}
+    {{Html::script('js/jquery.ba-bqq.min.js')}}
+    {{Html::script('js/jquery.jScrollPane.js')}}
+    {{Html::script('js/jquery.mousewheel.min.js')}}
+    {{Html::script('js/jquery.nivo.slider.pack.js')}}
+    {{Html::script('js/jquery.infieldlabel.min.js')}}
+    {{Html::script('js/jquery.carouFredSel.packed.js')}}
 
-        {{Html::script('js/linkify.js')}}
-        {{Html::script('js/jquery.min.js')}}
-        {{Html::script('js/jquery-migrate.min.js')}}
-        {{Html::script('js/jquery.easing.js')}}
-        {{Html::script('js/jquery.isotope.js')}}
-        {{Html::script('js/jquery.captify.js')}}
-        {{Html::script('js/jquery.blockUI.js')}}
-        {{Html::script('js/jquery.qtip.min.js')}}
-        {{Html::script('js/jquery.ba-bqq.min.js')}}
-        {{Html::script('js/jquery.jScrollPane.js')}}
-        {{Html::script('js/jquery.mousewheel.min.js')}}
-        {{Html::script('js/jquery.nivo.slider.pack.js')}}
-        {{Html::script('js/jquery.infieldlabel.min.js')}}
-        {{Html::script('js/jquery.carouFredSel.packed.js')}}
+    {{Html::script('js/jquery.fancybox.js')}}
+    {{Html::script('js/jquery.fancybox-media.js')}}
+    {{Html::script('js/jquery.fancybox-buttons.js')}}
 
-        {{Html::script('js/jquery.fancybox.js')}}
-        {{Html::script('js/jquery.fancybox-media.js')}}
-        {{Html::script('js/jquery.fancybox-buttons.js')}}
+    {{Html::script('js/script.js')}}
+    {{Html::script('js/cascade.js')}}
+</head>
 
-        {{Html::script('js/script.js')}}
-        {{Html::script('js/cascade.js')}}
-    </head>
+<body class="background-carbon">
 
-    <body class="background-carbon">
+<!-- Header -->
+<div class="header-wrapper">
 
-        <!-- Header -->
-        <div class="header-wrapper">
+    <div class="header main box-center clear-fix">
 
-            <div class="header main box-center clear-fix">
+        <div class="layout-4060 clear-fix">
 
-                <div class="layout-4060 clear-fix">
-
-                    <div class="layout-4060-left">
-                        <h1><a href="#!main">Yoichi Kaneko</a></h1>
-                        <h5><a href="#!main">freelance web developer</a></h5>
-                    </div>
-
-                    <div class="layout-4060-right">
-                        <ul class="no-list header-menu clear-fix">
-                            <li class="header-menu-mail"><a href="mailto:kaneko@php-zfex.jp">kaneko@php-zfex.jp</a></li>
-                        </ul>
-                    </div>
-
-                </div>
-
+            <div class="layout-4060-left">
+                <h1><a href="#!main">Yoichi Kaneko</a></h1>
+                <h5><a href="#!main">freelance web developer</a></h5>
             </div>
-        </div>
-        <!-- /Header -->
 
-
-        <!-- Content -->
-        <div class="content main box-center">
-
-            <!-- Cascade -->
-            <div class="cascade preloader">
-
-                <!-- Box menu -->
-                <ul class="cascade-menu">
-                    <li id="tab-1" class="lime-info">
-                        <a href="#!about">
-                            <span class="title">About</span>
-                            <span class="subtitle">About Me</span>
-                        </a>
-                    </li>
-                    <li id="tab-2" class="green-lightbulb">
-                        <a href="#!portfolio">
-                            <span class="title">Portfolio</span>
-                            <span class="subtitle">My Works</span>
-                        </a>
-                    </li>
-                    <li id="tab-3" class="orange-document">
-                        <a href="#!resume">
-                            <span class="title">Resume</span>
-                            <span class="subtitle">Personal Profile</span>
-                        </a>
-                    </li>
-                    <li id="tab-4" class="turquoise-arrow">
-                        <a href="#!private">
-                            <span class="title">My Private</span>
-                            <span class="subtitle">Free time</span>
-                        </a>
-                    </li>
+            <div class="layout-4060-right">
+                <ul class="no-list header-menu clear-fix">
+                    <li class="header-menu-mail"><a href="mailto:kaneko@php-zfex.jp">kaneko@php-zfex.jp</a></li>
                 </ul>
-                <!-- /Box menu -->
-
-                <!-- Window -->
-                <div class="cascade-window">
-
-                    <!-- Close bar -->
-                    <div class="cascade-window-close-bar">
-                        <a href="#!main"></a>
-                    </div>
-                    <!-- /Close bar -->
-
-                    <!-- Page content -->
-                    <div class="cascade-window-content">
-                        <div class="cascade-page clear-fix"></div>
-                    </div>
-                    <!-- /Page content -->
-
-                    <!-- Footer -->
-                    <div class="cascade-window-footer"></div>
-                    <!-- /Footer -->
-
-                </div>
-                <!-- /Window -->
-
-                <!-- Navigation -->
-                <a href="#" class="cascade-navigation cascade-navigation-prev"></a>
-                <a href="#" class="cascade-navigation cascade-navigation-next"></a>
-                <a href="#" class="cascade-navigation cascade-navigation-slider-prev"></a>
-                <a href="#" class="cascade-navigation cascade-navigation-slider-next"></a>
-                <!-- /Navigation -->
-
-            </div>
-            <!-- /Cascade -->
-
-        </div>
-        <!-- /Content -->
-
-
-        <!-- Footer -->
-        <div class="footer">
-
-            <hr class="footer-line" />
-
-            <div class="main box-center layout-7030 clear-fix">
-
-                <!-- Social icons -->
-                <div class="layout-7030-right">
-
-                    <ul class="no-list social-list-1">
-                        <li>Connect</li>
-                        <li><a href="#" class="social-rss"></a></li>
-                        <li><a href="#" class="social-facebook"></a></li>
-                        <li><a href="#" class="social-twitter"></a></li>
-                        <li><a href="#" class="social-google"></a></li>
-                        <li><a href="#" class="social-skype"></a></li>
-                    </ul>
-
-                </div>
-                <!-- /Social icons -->
-
             </div>
 
         </div>
-        <!-- /Footer -->
-        {{Html::script('js/page_config.js')}}
-        <script type="text/javascript">
-            $(document).ready(function()
-            {
-                $('.cascade').cascade(page,options,request);
-            });
-        </script>
 
-    </body>
+    </div>
+</div>
+<!-- /Header -->
+
+
+<!-- Content -->
+<div class="content main box-center">
+
+    <!-- Cascade -->
+    <div class="cascade preloader">
+
+        <!-- Box menu -->
+        <ul class="cascade-menu">
+            <li id="tab-1" class="lime-info">
+                <a href="#!about">
+                    <span class="title">About</span>
+                    <span class="subtitle">About Me</span>
+                </a>
+            </li>
+            <li id="tab-2" class="green-lightbulb">
+                <a href="#!portfolio">
+                    <span class="title">Portfolio</span>
+                    <span class="subtitle">My Works</span>
+                </a>
+            </li>
+            <li id="tab-3" class="orange-document">
+                <a href="#!resume">
+                    <span class="title">Resume</span>
+                    <span class="subtitle">Personal Profile</span>
+                </a>
+            </li>
+            <li id="tab-4" class="turquoise-arrow">
+                <a href="#!private">
+                    <span class="title">My Private</span>
+                    <span class="subtitle">Free time</span>
+                </a>
+            </li>
+        </ul>
+        <!-- /Box menu -->
+
+        <!-- Window -->
+        <div class="cascade-window">
+
+            <!-- Close bar -->
+            <div class="cascade-window-close-bar">
+                <a href="#!main"></a>
+            </div>
+            <!-- /Close bar -->
+
+            <!-- Page content -->
+            <div class="cascade-window-content">
+                <div class="cascade-page clear-fix"></div>
+            </div>
+            <!-- /Page content -->
+
+            <!-- Footer -->
+            <div class="cascade-window-footer"></div>
+            <!-- /Footer -->
+
+        </div>
+        <!-- /Window -->
+
+        <!-- Navigation -->
+        <a href="#" class="cascade-navigation cascade-navigation-prev"></a>
+        <a href="#" class="cascade-navigation cascade-navigation-next"></a>
+        <a href="#" class="cascade-navigation cascade-navigation-slider-prev"></a>
+        <a href="#" class="cascade-navigation cascade-navigation-slider-next"></a>
+        <!-- /Navigation -->
+
+    </div>
+    <!-- /Cascade -->
+
+</div>
+<!-- /Content -->
+
+
+<!-- Footer -->
+<div class="footer">
+    &nbsp;
+</div>
+<!-- /Footer -->
+{{Html::script('js/page_config.js')}}
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('.cascade').cascade(page, options, request);
+    });
+</script>
+
+</body>
 
 </html>
--- resources/views/page/private.blade.php
+++ resources/views/page/private.blade.php
@@ -3,21 +3,14 @@
         <li class="feature-list-icon-swarm">
             <h3>休日の外出</h3>
             <p>
-                Donec vestibulum lectus ut risus lacini tellus sollicitudin.
-                Sed vel sagittis nunc. Vestibulum massa arcu, scelerisque ut.
-            </p>
-        </li>
-        <li class="feature-list-icon-chat">
-            <h3>クラフトビール巡り</h3>
-            <p>
-                Morbi commodo posuere urna, sit am laoreet risus commodo a.
-                Curabitur in elit nisl, sed viverra quam. Aliquam eli.
+                休日には時間があれば外に出て、少し離れたところの神社を回ったり日帰り登山に行ったりしています。
             </p>
         </li>
         <li class="feature-list-icon-torii">
             <h3>神社巡り</h3>
             <p>
                 休日にはよく神社を回ります。御朱印目当てで行く事もありますし、無人の神社を調べてそれらを辿る事もあります。
+                大体関東の日帰りで行ける距離で、たまに小旅行で離れた場所に行く事もあります。
             </p>
         </li>
         <li class="feature-list-icon-mountain">
@@ -26,7 +19,13 @@
                 時間が取れた時には日帰りや前泊で登山に行く事があります。2018年より本格的に始めた趣味で、百名山だと白山登頂、天城山縦走、谷川岳縦走など達成しています。
             </p>
         </li>
-        <li class="feature-list-icon-basket">
+        <li class="feature-list-icon-untappd">
+            <h3>クラフトビール巡り</h3>
+            <p>
+                休日や、平日の仕事明けにビアバーに行って飲んで回っています。その記録はuhtappdというアプリで管理しています。untappd ID: straysheep
+            </p>
+        </li>
+        <li class="feature-list-icon-kosenjo">
             <h3>ソシャゲ</h3>
             <p>
                 Proin accumsan pretium suscipit torte. Pellentesque euismod tincidunt cursus.
