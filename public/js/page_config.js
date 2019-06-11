c7baba53b9a93cedddfb677b1beec97d7e7b779d
yoichi.kaneko <yoichi.kaneko@classi.jp>
2019-06-11 10:55:56 +0900

    デザイン調整途中

--- public/js/page_config.js
+++ public/js/page_config.js
@@ -1,41 +1,43 @@
+title = $('title').html();
+
 var page={
-        "about": {
-            "js": "",
-            "tab": "tab-1",
-            "html": "about",
-            "main": 1,
-            "title": "Cascade - Personal vCard Template - About Page"
-        },
-        "portfolio": {
-            "js": "",
-            "tab": "tab-2",
-            "html": "portfolio",
-            "main": 1,
-            "title": "Cascade - Personal vCard Template - Portfolio Page"
-        },
-        "resume": {
-            "js": "",
-            "tab": "tab-3",
-            "html": "resume",
-            "main": 1,
-            "title": "Cascade - Personal vCard Template - Resume Page"
-        },
-        "interests": {
-            "js": "",
-            "tab": "tab-4",
-            "html": "private",
-            "main": 1,
-            "title": "Cascade - Personal vCard Template - Interests Page"
-        }
-    };
+    "about": {
+        "js": "",
+        "tab": "tab-1",
+        "html": "about",
+        "main": 1,
+        "title": title + " - About"
+    },
+    "portfolio": {
+        "js": "",
+        "tab": "tab-2",
+        "html": "portfolio",
+        "main": 1,
+        "title": title + " - Portfolio"
+    },
+    "resume": {
+        "js": "",
+        "tab": "tab-3",
+        "html": "resume",
+        "main": 1,
+        "title": title + " - Resume"
+    },
+    "interests": {
+        "js": "",
+        "tab": "tab-4",
+        "html": "private",
+        "main": 1,
+        "title": title + " - Interests"
+    }
+};
 
 var options={
-        "openStart": "",
-        "title": "Cascade - Personal vCard Template",
-        "meta": {
-            "keywords": "black, clean, creative, curriculum, curriculum vitae, cv, dark, minimalist, personal, personal profile, personal vcard, portfolio, resume, vcard, virtual card",
-            "description": "Cascade is a minimalist personal vCard template based on four vertical menu tabs. The first tab is a typical descriptive about page with slider, the second is a gallery with build-in lightbox, next tab is a personal info page and the last tab is the contact page with working form."
-        }
-    };
+    "openStart": "",
+    "title": title,
+    //"meta": {
+    //    "keywords": "black, clean, creative, curriculum, curriculum vitae, cv, dark, minimalist, personal, personal profile, personal vcard, portfolio, resume, vcard, virtual card",
+    //    "description": "Cascade is a minimalist personal vCard template based on four vertical menu tabs. The first tab is a typical descriptive about page with slider, the second is a gallery with build-in lightbox, next tab is a personal info page and the last tab is the contact page with working form."
+    //}
+};
 
 var request='';
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja">
     <head>
-        <title>Cascade - Personal vCard Template</title>
+        <title>Yoichi Kaneko's Portfolio Page</title>
         <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
-        <meta name="keywords" content="black, clean, creative, curriculum, curriculum vitae, cv, dark, minimalist, personal, personal profile, personal vcard, portfolio, resume, vcard, virtual card"/>
-        <meta name="description" content="Cascade is a minimalist personal vCard template based on four vertical menu tabs. The first tab is a typical descriptive about page with slider, the second is a gallery with build-in lightbox, next tab is a personal info page and the last tab is the contact page with working form."/>
+        <meta name="keywords" content="portfolio, developer, php, freelance"/>
+        <meta name="description" content="金子陽一のポートフォリオページ"/>
 
         {{Html::style('css/nivo-slider.css')}}
         {{Html::style('css/jquery.qtip.css')}}
@@ -15,9 +15,6 @@
         {{Html::style('css/fancybox/helpers/jquery.fancybox-buttons.css')}}
         {{Html::style('css/base.css')}}
         {{Html::style('css/page.css')}}
-
-        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300"/>
-        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Share" />
 
         {{Html::style('css/responsive/width-0-959.css', ['media' => 'screen and (min-width:0px) and (max-width:959px)'])}}
         {{Html::style('css/responsive/width-0-767.css', ['media' => 'screen and (min-width:0px) and (max-width:767px)'])}}
@@ -45,7 +42,6 @@
         {{Html::script('js/jquery.nivo.slider.pack.js')}}
         {{Html::script('js/jquery.infieldlabel.min.js')}}
         {{Html::script('js/jquery.carouFredSel.packed.js')}}
-        <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
 
         {{Html::script('js/jquery.fancybox.js')}}
         {{Html::script('js/jquery.fancybox-media.js')}}
@@ -53,7 +49,6 @@
 
         {{Html::script('js/script.js')}}
         {{Html::script('js/cascade.js')}}
-
     </head>
 
     <body class="background-carbon">
@@ -66,15 +61,13 @@
                 <div class="layout-4060 clear-fix">
 
                     <div class="layout-4060-left">
-                        <h1><a href="#!main">Anna Brown</a> </h1>
-                        <h5><a href="#!main">professional photographer &amp; web developer</a></h5>
+                        <h1><a href="#!main">Yoichi Kaneko</a></h1>
+                        <h5><a href="#!main">freelance web developer</a></h5>
                     </div>
 
                     <div class="layout-4060-right">
                         <ul class="no-list header-menu clear-fix">
-                            <li class="header-menu-download"><a href="#">Download my vCard</a></li>
-                            <li class="header-menu-phone">1.800.353.252</li>
-                            <li class="header-menu-mail"><a href="#">anna.brown@mail.com</a></li>
+                            <li class="header-menu-mail"><a href="mailto:kaneko@php-zfex.jp">kaneko@php-zfex.jp</a></li>
                         </ul>
                     </div>
 
@@ -93,25 +86,25 @@
 
                 <!-- Box menu -->
                 <ul class="cascade-menu">
-                    <li id="tab-1" class="blue-info">
+                    <li id="tab-1" class="lime-info">
                         <a href="#!about">
                             <span class="title">About</span>
                             <span class="subtitle">About Me</span>
                         </a>
                     </li>
-                    <li id="tab-2" class="lime-camera">
+                    <li id="tab-2" class="green-lightbulb">
                         <a href="#!portfolio">
                             <span class="title">Portfolio</span>
                             <span class="subtitle">My Works</span>
                         </a>
                     </li>
-                    <li id="tab-3" class="yellow-document">
+                    <li id="tab-3" class="orange-document">
                         <a href="#!resume">
                             <span class="title">Resume</span>
                             <span class="subtitle">Personal Profile</span>
                         </a>
                     </li>
-                    <li id="tab-4" class="green-people">
+                    <li id="tab-4" class="turquoise-arrow">
                         <a href="#!private">
                             <span class="title">My Interests</span>
                             <span class="subtitle">Free time</span>
