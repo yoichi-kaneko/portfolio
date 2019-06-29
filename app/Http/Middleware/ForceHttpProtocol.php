e5555384d84df508f8dbf3d2f430bfe10a0e7978
金子陽一 <kaneko@php-zfex.jp>
2019-06-29 21:53:03 +0900

    AWS用のhttps転送を実装

--- app/Http/Middleware/ForceHttpProtocol.php
+++ app/Http/Middleware/ForceHttpProtocol.php
@@ -9,7 +9,9 @@ class ForceHttpProtocol {
     public function handle($request, Closure $next)
     {
         if (!$request->secure() && env('APP_ENV') === 'production') { // 本番環境のみ常時SSL化する
-            return redirect()->secure($request->getRequestUri());
+            if ($_SERVER["HTTP_X_FORWARDED_PROTO"] != 'https') {
+                return redirect()->secure($request->getRequestUri());
+            }
         }
 
         return $next($request);
