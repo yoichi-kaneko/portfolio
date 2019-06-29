01a539ba4fd5c1f19dbebb36f5b6d7e96aa15280
金子陽一 <kaneko@php-zfex.jp>
2019-06-29 21:42:35 +0900

    ポートフォリオのリンクを調整

--- resources/views/page/portfolio.blade.php
+++ resources/views/page/portfolio.blade.php
@@ -3,7 +3,7 @@
         <ul class="no-list gallery-list clear-fix">
             @foreach ($portfolios as $portfolio)
                 <li class="@if($loop->index % 3 == 0) left @elseif($loop->index % 3 == 1) center @else right @endif filter-filter-1">
-                    <a class="fancybox-iframe image-preloader" href="{{ asset('/portfolio/' . $portfolio->id) }}/">
+                    <a class="fancybox-iframe image-preloader" href="/portfolio/{{ $portfolio->id }}" >
                         <img src="/image/thumb/{{ $portfolio->code }}.jpg" alt="{{ $portfolio->name }}"/>
                 </a>
             </li>
