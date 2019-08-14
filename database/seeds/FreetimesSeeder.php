d3c8f7122183bbc3750270899094ac65c50ba8fa
yoichi-kaneko <kaneko@php-zfex.jp>
2019-08-14 09:44:40 +0900

    DBスキーマの更新。urlを追加

--- database/seeds/FreetimesSeeder.php
+++ database/seeds/FreetimesSeeder.php
@@ -22,26 +22,31 @@ class FreetimesSeeder extends Seeder
                 'code' => 'swarm',
                 'name' => '休日の外出',
                 'description' => '休日には時間があれば外に出て、少し離れたところの神社を回ったり日帰り登山に行ったりしています。',
+                'url' => null
             ],
             [
                 'code' => 'torii',
                 'name' => '神社巡り',
                 'description' => '休日にはよく神社を回ります。御朱印目当てで行く事もありますし、無人の神社を調べてそれらを辿る事もあります。大体関東の日帰りで行ける距離で、たまに小旅行で離れた場所に行く事もあります。',
+                'url' => null
             ],
             [
                 'code' => 'mountain',
                 'name' => '登山',
                 'description' => '時間が取れた時には日帰りや前泊で登山に行く事があります。2018年より本格的に始めた趣味で、百名山だと白山登頂、天城山縦走、谷川岳登頂など達成しています。',
+                'url' => null
             ],
             [
                 'code' => 'untappd',
                 'name' => 'クラフトビール巡り',
                 'description' => '休日や、平日の仕事明けにビアバーに行って飲んで回っています。その記録はuhtappdというアプリで管理しています。untappd ID: straysheep',
+                'url' => null
             ],
             [
                 'code' => 'kosenjo',
                 'name' => 'ソシャゲ',
                 'description' => '他にはソシャゲでグラブルをプレイしていたりしています。',
+                'url' => null
             ],
         ];
     }
