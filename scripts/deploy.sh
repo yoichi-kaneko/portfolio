3d8269f42fb84a6490cbb7e0dc3b0a739241c81e
yoichi-kaneko <kaneko@webstudio-wanderlust.voyage>
2019-11-30 19:24:40 +0900

    パスの調整

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd `dirname $0`/..
 sudo git pull
-COMPOSER_PATH=`type composer`
+COMPOSER_PATH=`which composer`
 sudo $COMPOSER_PATH install
 php artisan migrate:refresh --force
 php artisan db:seed --force
