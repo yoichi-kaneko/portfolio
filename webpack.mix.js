4bfaf2a3fb384b8256e74743fe55c4377c07f096
yoichi-kaneko <kaneko@webstudio-wanderlust.voyage>
2019-12-04 17:01:24 +0900

    jsファイルをnpmに移動

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,3 +18,22 @@ glob.sync('resources/sass/responsive/*.scss').map(function(file) {
 });
 mix.copyDirectory('resources/assets/image', 'public/image');
 
+mix.js('resources/js/linkify.js', 'public/js')
+    .js('resources/js/jquery.min.js', 'public/js')
+    .js('resources/js/jquery-migrate.min.js', 'public/js')
+    .js('resources/js/jquery.easing.js', 'public/js')
+    .js('resources/js/jquery.isotope.js', 'public/js')
+    .js('resources/js/jquery.captify.js', 'public/js')
+    .js('resources/js/jquery.blockUI.js', 'public/js')
+    .js('resources/js/jquery.qtip.min.js', 'public/js')
+    .js('resources/js/jquery.ba-bqq.min.js', 'public/js')
+    .js('resources/js/jquery.jScrollPane.js', 'public/js')
+    .js('resources/js/jquery.mousewheel.min.js', 'public/js')
+    .js('resources/js/jquery.nivo.slider.pack.js', 'public/js')
+    .js('resources/js/jquery.infieldlabel.min.js', 'public/js')
+    .js('resources/js/jquery.carouFredSel.packed.js', 'public/js')
+    .js('resources/js/jquery.fancybox.js', 'public/js')
+    .js('resources/js/jquery.fancybox-media.js', 'public/js')
+    .js('resources/js/jquery.fancybox-buttons.js', 'public/js')
+    .js('resources/js/script.js', 'public/js')
+    .js('resources/js/cascade.js', 'public/js');
