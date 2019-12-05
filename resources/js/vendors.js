8c9a411eb5f4fc68eaaeb4162370415bd42c26aa
yoichi-kaneko <kaneko@webstudio-wanderlust.voyage>
2019-12-05 12:40:57 +0900

    npmに移行途中

--- resources/js/vendors.js
+++ resources/js/vendors.js
@@ -1,6 +1,9 @@
 global.$ = global.jQuery = require('jquery');
 
 require('linkifyjs');
+require('infieldLabel');
 
 import 'qtip2/dist/jquery.qtip';
 import 'jscrollpane/script/jquery.jscrollpane';
+import 'jquery-mousewheel';
+
