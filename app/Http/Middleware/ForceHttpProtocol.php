421be544a138a35c23221729ed36c87fa244cad9
yoichi-kaneko <kaneko@webstudio-wanderlust.voyage>
2020-01-16 20:44:41 +0900

    HTTP_X_FORWARDED_PROTOがない場合に対応

--- app/Http/Middleware/ForceHttpProtocol.php
+++ app/Http/Middleware/ForceHttpProtocol.php
@@ -9,7 +9,7 @@ class ForceHttpProtocol {
     public function handle($request, Closure $next)
     {
         if (!$request->secure() && env('APP_ENV') === 'production') { // 本番環境のみ常時SSL化する
-            if ($_SERVER["HTTP_X_FORWARDED_PROTO"] != 'https') {
+            if (!empty($_SERVER["HTTP_X_FORWARDED_PROTO"]) && $_SERVER["HTTP_X_FORWARDED_PROTO"] != 'https') {
                 return redirect()->secure($request->getRequestUri());
             }
         }
