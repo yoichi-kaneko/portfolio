f57d80e5788422f3a7a2bcacc0449b88769f216f
yoichi-kaneko <kaneko@webstudio-wanderlust.voyage>
2020-08-29 11:29:13 +0900

    deploy処理からsudoを除外

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 cd `dirname $0`/..
-sudo git pull
-COMPOSER_PATH=`which composer`
-sudo $COMPOSER_PATH install --no-dev
-sudo npm install
-sudo npm run prod
+git pull
+composer install --no-dev
+npm install
+npm run prod
 php artisan migrate:refresh --force
 php artisan db:seed --force
